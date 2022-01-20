475659d2ee760ecef8e69728134e68897d6d4aeb
fernando_videira <2211877@my.ipleiria.pt>
2022-01-20 20:55:02 +0000

    Search Bar Pronta

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ var currencySymbol;
 currency = "eur";
 currencySymbol = "€"
 
+$('#search').on("change", verifyIfEmpty);
+
 function apiCall() {
     $('.clone').remove();
     $.ajax({
@@ -37,9 +39,11 @@ function apiCall() {
 
             if (variation.match("^-")) {
                 $(".variation", rowClone).css("color", "red");
+                $(".variation", rowClone).prepend("🡻 ");
             }
             else {
                 $(".variation", rowClone).css("color", "green");
+                $(".variation", rowClone).prepend("🡹 ");
             }
         })
     })
@@ -67,7 +71,52 @@ $('#currencylist li').on('click', function () {
     apiCall();
 })
 
+$('#btnSearch').on('click', function searchFunction() {
+    var valTosearch = $('#search').val().toLowerCase();
+
+    $.ajax({
+        type: 'GET',
+        datatype: 'json',
+        url: "https://api.coingecko.com/api/v3/coins/" + valTosearch,
+    }).done(function (result) {
+
+        //console.log(result);
+
+        $('.clone').hide();
+        $('#row').show();
+
+        $('.redirect').attr('onclick', `redirect('${result.id}')`)
+        $('.rank').text(result.market_cap_rank)
+        $('.nome').text(result.name + " (" + result.symbol.toUpperCase() + ")")
+        $('.symbol').attr('src', result.image.small)
+        $('.price').text(currencySymbol + " " + result.market_data.current_price[currency])
+        $('.marketcap').text(result.market_data.market_cap[currency])
+        $('.variation').text(result.market_data.price_change_percentage_24h.toFixed(2))
+        $('.volume24h').text(result.market_data.circulating_supply)
+
+        $('.like-btn').attr('id', result.id).attr('onclick', 'favoritos(this)');
+
+        var variation = $('.variation').text()
+
+        if (variation.match("^-")) {
+            $(".variation").css("color", "red");
+            $(".variation").prepend("🡻 ");
+        } else {
+            $(".variation").css("color", "green");
+            $(".variation").prepend("🡹 ");
+        }
+    })
+})
+
 window.onload = apiCall
+
+function verifyIfEmpty() {
+    if ($('#table #row > :visible').length == 8) {
+        if ($('#search').val().length == 0) {
+            apiCall();
+        }
+    }
+}
 
 function favoritos(moeda) {
     $(moeda).toggleClass("favoritos")
